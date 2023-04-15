486c0e13ba5114e2aae36d72b2d1adc18096b86c
Mohan Narayanaswamy <mohanmca@gmail.com>
2023-04-15 23:19:19 +0000

    exercise completed!

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,39 +1,16 @@
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
+ select * from {{ ref('stg_customers') }}
 ),
-
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
+    select * from {{ ref('stg_orders') }}
 ),
-
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
-
+    from orders group by 1
 ),
 
 final as (
