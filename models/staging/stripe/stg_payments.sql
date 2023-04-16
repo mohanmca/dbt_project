1e3cd093ac7ddcd7e14461ce965a03e237b64ad0
Mohan Narayanaswamy <mohanmca@gmail.com>
2023-04-16 01:56:02 +0000

    Completion of sources!

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,14 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
+with staged_payment as (
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
 
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at
 
-from raw.stripe.payment 
+    from {{source('stripe', 'payment')}}
+)
+select * from staged_payment
