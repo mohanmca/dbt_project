45c2402af2edb8d114f3e533f73cfbd992748e36
Mohan Narayanaswamy <mohanmca@gmail.com>
2023-04-16 00:52:17 +0000

    dbt source freshness

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,10 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-
-from raw.jaffle_shop.customers
+with source as (
+    select     *    from {{source('jaffle_shop', 'customers')}}
+),
+staged as (
+    select id as customer_id, 
+           first_name, 
+           last_name 
+    from source
+)
+select * from staged
